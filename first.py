66e4b624cc66a8f1f32cf09b692556cc6306c60d
Dawa Wangdi <02230211.cst@rub.edu.bt>
2024-02-18 14:47:04 +0530

    changes made

--- first.py
+++ first.py
@@ -2,3 +2,10 @@ print ("hello world")
 print("I am as cool as ICE")
 print ("wow")
 
+age = 154
+if age >120:
+    print ("Invailaid age")
+elif age >= 18:
+    print("elegible to vote")
+else:
+    print("not elegible to vote")
