01f08e45bdf8b83299deabc4c4341fb67d0e28d4
Dawa Wangdi <02230211.cst@rub.edu.bt>
2024-03-04 20:40:18 +0530

    Class activities

--- task_1.py
+++ task_1.py
@@ -1,10 +1,14 @@
 Age = int (input ("Enter your age:"))
 std = input ("Are you student? (yes/no):")
 
-if Age <= 12 and std =="yes":
+if Age <= 12 and (std =="yes"):
     print("You are eligible for a discount on ticket.")
-elif Age > 12 or std == "no":
+elif Age <=18 and(Age>13 and std == "yes" ):
+    print("you are  not eligible for a discount on ticket.")
+    
+elif Age > 12 and (std == "no"):
     print("sorry, you are  not eligible for a discount on ticket.")
+
 else :
     print("Entered input is invaild")
 
