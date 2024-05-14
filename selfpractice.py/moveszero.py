3bf3ba2be951e29a2b69a0d70ef3e7a7a2e03e2d
Dawa Wangdi <02230211.cst@rub.edu.bt>
2024-05-14 20:19:01 +0530

    zero movers

---
+++ selfpractice.py/moveszero.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        #This code implements an algorithm to move all zeros in a list of integers (nums) to the end 
+        #while maintaining the relative order of non-zero elements
+        l = 0  
+        #l = 0: Initializes a variable l to 0. This will be used as the left pointer 
+        for r in range(len(nums)):
+            #r right pointer, iterates over len of num list.
+            if nums[r]:
+                #if nums[r]:: Checks if the current element at index r is non-zero
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+
+        return nums
+    
+sol = Solution ()
+nums = [3,4,0,7,8,0,6,78,900,76,00]
+print(sol.moveZeroes(nums))
+
+
+
+
+           
