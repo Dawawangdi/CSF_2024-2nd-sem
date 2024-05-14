5111ffa88e17401649b18def7f75b92463791301
Dawa Wangdi <02230211.cst@rub.edu.bt>
2024-05-14 23:19:06 +0530

    Subsequences

---
+++ selfpractice.py/subsequence.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i ,j = 0,0
+        while i<len(s) and j<len(t):
+            if s[i] == t[j]:
+                i+=1
+                j+=1
+            else:
+                j+=1
+
+        if i == len(s):
+            print (True) 
+        else:
+            print(False)
+
+        return(s,t)
+    
+sol = Solution()
+s = ('amc')
+t= ('liahybbfjtutic')
+print(sol.isSubsequence(s,t))
+
+
+
+
