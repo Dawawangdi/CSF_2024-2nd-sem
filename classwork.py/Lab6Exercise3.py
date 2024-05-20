c79d2fa707f78f628f3c18748e6f5544f122d24e
Dawa Wangdi <02230211.cst@rub.edu.bt>
2024-05-20 14:28:07 +0530

    Lab_6.Exercise.3

---
+++ classwork.py/Lab6Exercise3.py
@@ -0,0 +1,12 @@
+##Exercise 3: Printing Numbers with Skipping and Breaking
+
+# Print numbers from 1 to 9, skipping 3 in the inner loop and breaking when reaching 7
+print("Printing numbers from 1 to 9 with skipping 3 and breaking at 7:")
+for i in range(1, 10):
+    if i == 7:
+        break
+    if i == 3:
+        continue
+    print(i)
+
+
