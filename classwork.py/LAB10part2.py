217f8a16b11fa669c7d7ac89d2f87f0d7c5ab3bd
Dawa Wangdi <02230211.cst@rub.edu.bt>
2024-05-20 16:05:09 +0530

    Lab10_Merge Two sorted Lists

---
+++ classwork.py/LAB10part2.py
@@ -0,0 +1,24 @@
+###Merge Two Sorted Lists:
+
+class Solution:
+    # Function to merge two sorted lists
+    def mergeTwoLists(self, list1, list2):
+        # Initialize a dummy node
+        dummy = ListNode(-1)
+        current = dummy
+        
+        # Merge lists while both have nodes
+        while list1 and list2:
+            # Select smaller value node
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next  # Move to next node
+            
+        # Append remaining nodes
+        current.next = list1 or list2
+        
+        return dummy.next  # Return merged list head
